a24f8007808ace3cedad19c2da8d7591d0398b42
ryeong <tvxqn7@daum.net>
2019-01-07 16:49:31 +0900

    190107 jupyter_notebook

--- Baekjoon/step1_print.py
+++ Baekjoon/step1_print.py
@@ -5,17 +5,17 @@
 
 # 2389. 배달하는 봉지의 최소 개수 출력
 
-bag = int(input())
+N = int(input())
 
-five, f_remainder = divmod(bag, 5)
+five, f_remainder = divmod(N, 5)
 three, t_remainder = divmod(f_remainder, 3)
 
 if t_remainder == 0:
-    print(five, three)
-elif (f_remainder + 5) % 3 == 0:
-    print(five-1, (t_remainder + 5)//3+1)
+    print(five + three)
+elif ((f_remainder + 5) % 3 == 0) and (five != 0):
+    print((five-1) + (f_remainder + 5)//3)
 elif t_remainder % 3 == 0:
-    print(five, three)
+    print(five + three)
 else:
     print(-1)
 
