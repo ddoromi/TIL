2b9ce8195f1acc8122e56294b2a74eff671a02c8
seongryeong <22.ryeong@gmail.com>
2019-03-01 20:57:54 +0900

    190301

---
+++ Algorithm/06_Queue/workshop.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('workshop.txt')
+
+T = int(input())
+for test_case in range(T):
+    P, Q, R, S, W = map(int, input().split())
+    a_fee = W * P
+    
+    if W <= R:
+        b_fee = Q
+    else:
+        b_fee = Q + (W - R) * S
+    
+    if a_fee > b_fee:
+        result = b_fee
+    else:
+        result = a_fee
+    
+    print(f'#{test_case + 1} {result}')
