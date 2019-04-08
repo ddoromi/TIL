2aff320f5b6ce70058ed3929e1f403e86a3cd53e
ryeong <tvxqn7@daum.net>
2019-04-08 14:59:45 +0900

    finish urls, models

---
+++ Programming/07_Django/review_prj/board/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+app_name = 'board'
+
+urlpatterns = [
+    path('index/', views.index),
+    path('greeting/<name>/<str:role>', views.greeting),
+    # create
+    path('articles/new', views.article_new),
+    path('articles/create', views.article_create),
+    path('articles', views.article_list),
+    path('articles/<int:id>', views.article_detail),
+    path('articles/<int:id>/edit', views.article_edit),
+    path('articles/<int:id>/update', views.article_update),
+    path('articles/<int:id>/delete', views.article_delete),
+]
