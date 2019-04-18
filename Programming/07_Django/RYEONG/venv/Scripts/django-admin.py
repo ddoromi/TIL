1aa5471f4ef4feb7e0161d1097d0bcc6f3754f73
ryeong <tvxqn7@daum.net>
2019-04-18 20:13:43 +0900

    dfdfdfd

---
+++ Programming/07_Django/RYEONG/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\student\SSAFY\TIL\Programming\07_Django\RYEONG\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
